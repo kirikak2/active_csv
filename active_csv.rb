83669a86f2be5c5353e84576714780bae20d6c9d
Marco Antonio <marcofognog@gmail.com>
2011-03-01 14:30:15 -0300

    refatoração

--- active_csv.rb
+++ active_csv.rb
@@ -1,28 +1,62 @@
-class ActiveCSV
-	require 'csv'
-	require 'yaml'
+class AttrFile
+	attr_accessor :name
 
-	attr_accessor :db_file_name, :id, :attr_file_name
-
-	def initialize
-		self.db_file_name = self.class.to_s.downcase+".txt"
-		self.id = next_id 
-		self.attr_file_name = "models/attributes.yml"
+	def initialize(_name)
+		@name = _name
+	end
+	def fields(model_name)
+		# The sequency of the attributes is always the same as in the yaml file.
+		model_fields = YAML.load_file(@name)
+		model_fields[model_name].split(', ') #Array
 	end
 
-	def self.all
+end
+
+class DbFile
+
+	attr_accessor :name
+
+	def initialize(_name)
+		@name = _name
+	end
+	def csv_content
+		csv_rows = Array.new
+		if !file_exists?
+			create_db_file
+		end
+		CSV.read(name).each do |row|
+				csv_rows << row
+		end
+		csv_rows
+	end
+
+	def file_exists?
+		File.exists?(name)
+	end
+
+	def create_db_file
+		CSV.open(name,'w')
+	end
+
+	def field_values
+		ActiveCSV.field_values
+	end
+end
+
+module ClassMethods
+	def all
 		objects = Array.new
-		new_object = eval(self.name).new # aqruivo de bd sempre será o nome do modelo no plural (regras do inglês)
-		attr_array = new_object.csv_content
+		db_file = DbFile.new("car"+".txt")
+		attr_array = db_file.csv_content
 		objects = ar_to_obj(attr_array)
 		objects
 	end
 
-	def self.ar_to_obj(attr_array) # turns array of arrays into object properties. Returns array of objects
+	def ar_to_obj(attr_array) # turns array of arrays into object properties. Returns array of objects
 		objects = Array.new
-		new_object = eval(self.name).new#  
+		new_object = eval(self.name).new
 		attr_array.each do |row|
-				fields_ar = new_object.fields
+				fields_ar = new_object.attr_file.fields(model_name)
 				fields_ar.each_index do |index|
 					eval("new_object."+fields_ar[index]+" = row[index]") #new_object.fields_ar[0] = row[0] and so on...
 				end
@@ -32,24 +66,23 @@ class ActiveCSV
 	end
 
 # relacionadas ao find
-	def self.check_attr?(attribute)
-		object  = eval(self.name).new
-		object.fields.include? attribute
+	def check_attr?(attribute)
+		attr_file = AttrFile.new("models/attributes.yml")	
+		attr_file.fields(model_name).include? attribute
 	end
 
-	def self.attr_column(attribute)
+	def attr_column(attribute)
 		object  = eval(self.name).new
-		object.fields.index attribute
+		object.attr_file.fields(model_name).index attribute
 	end
 
-	def self.find(*args)
+	def find(*args)
 		found_rows = Array.new
 		args[0].keys.each do |key|
 			if check_attr? key.to_s
+				file = DbFile.new("car.txt")
 				col = attr_column key.to_s # descobrir qual é a posição deste atributo
-				object =  eval(self.name).new
-				object.db_file_name = self.name.downcase+".txt"
-				content = object.csv_content #pegar o conteudo do arquivo
+				content = file.csv_content # pegar o conteudo do arquivo
 				found_cols = Array.new
 				content.each_with_index do |row, index|
 					if row[col] == args[0][key]				#achar a palavra de busca dentro do arquivo
@@ -70,44 +103,39 @@ class ActiveCSV
 	end
 
 	def model_name
+		self.name.to_s.downcase
+	end
+end
+
+class ActiveCSV
+
+	require 'csv'
+	require 'yaml'
+
+	extend ClassMethods
+
+	attr_accessor :id, :attr_file, :db_file
+
+	def initialize
+		self.attr_file = AttrFile.new("models/attributes.yml")
+		self.db_file = DbFile.new("car"+".txt")
+		self.id = next_id 
+	end
+
+	def model_name
 		self.class.to_s.downcase
 	end
 	
-	def fields
-		# The sequency of the attributes is always the same as in the yaml file.
-		model_fields = YAML.load_file(attr_file_name)
-		model_fields[model_name].split(', ') #Array
-	end
-
-	def field_values(model)
+	def field_values
 		attributes = Array.new
-		fields.each do |field|
+		attr_file.fields(model_name).each do |field|
 			 attributes << eval(field)
 		end
 		attributes #String
 	end
 
-	def file_exists?
-		File.exists?(db_file_name)
-	end
-
-	def create_db_file
-		CSV.open(db_file_name,'w')
-	end
-
-	def csv_content
-		csv_rows = Array.new
-		if !file_exists?
-			create_db_file
-		end
-		CSV.read(db_file_name).each do |row|
-				csv_rows << row
-		end
-		csv_rows
-	end
-
 	def last_id
-		attr_array = csv_content
+		attr_array = self.db_file.csv_content
 		unless attr_array.count == 0
 			attr_array.last[0]
 		else
@@ -120,9 +148,9 @@ class ActiveCSV
 	end
 
 	def save
-		a = csv_content
-		new_row = field_values(model_name)
-		CSV.open(db_file_name,"wb") do |csv|		
+		a = db_file.csv_content
+		new_row = field_values
+		CSV.open(db_file.name,"wb") do |csv|		
 				a.each do |line|
 					csv << line
 				end
--- test_active_csv.rb
+++ test_active_csv.rb
@@ -10,25 +10,23 @@ class ActiveCSVTest < Test::Unit::TestCase
 ############ SAVE method #################
 	def test_save_file_exists 
 		car = Car.new
-		car.attr_file_name = "models/attributes.yml"
 		car.year = "1998"
 		car.color = "red"
 		car.brand = "wolksvagen"
 		car.save
-		assert File.exists? car.db_file_name
-		File.delete(car.db_file_name)
+		assert File.exists? car.db_file.name
+		File.delete(car.db_file.name)
 	end
 
 	def test_save_quantity
 		create_sample_file
 		before = Car.all.length
 		car = Car.new
-		car.attr_file_name = "models/attributes.yml"
 		car.year = "1998"
 		car.color = "red"
 		car.brand = "wolksvagen"
 		car.save
-		assert_equal Car.all.length,before+1
+		assert_equal Car.all.length, before+1
 		delete_sample
 	end
 
