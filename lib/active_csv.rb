e38518b3f000560385570f004b18f0fbfc5e5ace
Marco Antonio <marcofognog@gmail.com>
2011-03-05 15:56:06 -0300

    class dbfile separedted from main file

--- lib/active_csv.rb
+++ lib/active_csv.rb
@@ -1,36 +1,7 @@
 require 'active_csv/errors'
 require 'active_csv/class_methods'
 require 'active_csv/attr_file'
-
-class DbFile
-	attr_accessor :name
-
-	def initialize(_name)
-		@name = _name
-	end
-	def csv_content
-		csv_rows = Array.new
-		if !file_exists?
-			create_db_file
-		end
-		CSV.read(name).each do |row|
-				csv_rows << row
-		end
-		csv_rows
-	end
-
-	def file_exists?
-		File.exists?(name)
-	end
-
-	def create_db_file
-		CSV.open(name,'w')
-	end
-
-	def field_values
-		ActiveCSV.field_values
-	end
-end
+require 'active_csv/db_file'
 
 class ActiveCSV
 	require 'csv'
