fac5968d551784601cd301abc0e5fc8dbe144450
Marco Antonio <marcofognog@gmail.com>
2011-03-05 23:48:28 -0300

    now added - initialize now accepts hash as arguments ehi attr

--- lib/active_csv.rb
+++ lib/active_csv.rb
@@ -11,7 +11,10 @@ class ActiveCSV
 
 	attr_accessor :id, :attr_file, :db_file
 
-	def initialize()
+	def initialize(*args)	
+		if args[0].class == Hash
+			hash_to_obj(args[0])
+		end
 		self.attr_file = AttrFile.new("models/attributes.yml")
 		self.db_file = DbFile.new(model_name+".txt")
 	end
