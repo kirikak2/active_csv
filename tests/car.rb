4a73c5f96a4909955fc8ac8b3317b40e11f155a5
marcofognog <marcofognog@gmail.com>
2011-03-27 12:49:49 -0300

    some files removed

--- tests/car.rb
+++
@@ -1,6 +0,0 @@
-require 'active_csv'
-
-class Car < ActiveCSV
-	attr_accessor :color, :year, :brand
-	validates_presence_of :brand
-end
--- tests/car_with_set_attr.rb
+++
@@ -1,7 +0,0 @@
-require 'active_csv'
-
-class CarWithSetAttr < ActiveCSV
-	attr_accessor :color, :year, :brand
-	validates_presence_of :brand
-	set_attr_file_name "config/csv_attributes_with_other_name.yml"
-end
